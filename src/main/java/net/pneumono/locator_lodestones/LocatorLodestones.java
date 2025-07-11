44d036a50c97c270c9fbafd22352d84237a19591
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-07-11 05:59:52 +0100

    Initial commit

---
+++ src/main/java/net/pneumono/locator_lodestones/LocatorLodestones.java
@@ -0,0 +1,18 @@
+package net.pneumono.locator_lodestones;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LocatorLodestones implements ClientModInitializer {
+	public static final String MOD_ID = "locator_lodestones";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("Locator Lodestones");
+
+	@Override
+	public void onInitializeClient() {
+		LOGGER.info("Initializing Locator Lodestones");
+	}
+}
