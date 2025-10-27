a16e26df9e29b4c652103a99833a2149de85d614
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-10-27 15:21:26 +0000

    Fix waypoints not updating when items are dropped

--- src/main/java/net/pneumono/locator_lodestones/mixin/PlayerInventoryMixin.java
+++ src/main/java/net/pneumono/locator_lodestones/mixin/PlayerInventoryMixin.java
@@ -3,6 +3,7 @@ package net.pneumono.locator_lodestones.mixin;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
 import net.pneumono.locator_lodestones.WaypointTracking;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,6 +11,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerInventory.class)
 public abstract class PlayerInventoryMixin {
@@ -20,6 +22,16 @@ public abstract class PlayerInventoryMixin {
             at = @At("RETURN")
     )
     private void updateWaypoints(CallbackInfo ci) {
+        if (this.player instanceof ClientPlayerEntity) {
+            WaypointTracking.markWaypointsDirty();
+        }
+    }
+
+    @Inject(
+            method = "dropSelectedItem",
+            at = @At("RETURN")
+    )
+    private void updateWaypointsOnItemDrop(boolean entireStack, CallbackInfoReturnable<ItemStack> cir) {
         if (this.player instanceof ClientPlayerEntity) {
             WaypointTracking.markWaypointsDirty();
         }
