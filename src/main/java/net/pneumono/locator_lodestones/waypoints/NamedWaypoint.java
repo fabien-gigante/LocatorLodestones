dfffa393172681a6f36786b3d5530a46af606a5c
fabien-gigante <fabien.gigante@free.fr>
2025-11-10 23:46:59 +0100

    Implement INamed interface in NamedWaypoint class and add name getter/setter methods

--- src/main/java/net/pneumono/locator_lodestones/waypoints/NamedWaypoint.java
+++ src/main/java/net/pneumono/locator_lodestones/waypoints/NamedWaypoint.java
@@ -12,8 +12,9 @@ import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.waypoint.TrackedWaypoint;
 import net.minecraft.world.waypoint.Waypoint;
 import net.minecraft.world.waypoint.WaypointStyle;
+import net.pneumono.locator_lodestones.INamed;
 
-public class NamedWaypoint extends TrackedWaypoint.Positional {
+public class NamedWaypoint extends TrackedWaypoint.Positional implements INamed {
     protected Optional<Text> name = Optional.empty();
 
     protected NamedWaypoint(String source, Config config, Vec3i pos, Optional<Text> name) {
@@ -24,7 +25,10 @@ public class NamedWaypoint extends TrackedWaypoint.Positional {
         this(source, configFromStyle(style, color), pos, name);
     }
 
+    @Override
     public Optional<Text> getName() { return name; }
+    @Override
+    public void setName(Optional<Text> name) { this.name = name; }
 
     private static Config configFromStyle(RegistryKey<WaypointStyle> style, @Nullable Integer color) {
         Waypoint.Config config = new Waypoint.Config();
