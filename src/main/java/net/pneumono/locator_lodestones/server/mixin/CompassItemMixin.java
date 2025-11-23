c186104c2e003935bef60a277266a25a62f6c27c
fabien-gigante <fabien.gigante@free.fr>
2025-11-23 11:34:49 +0100

    Refactor hue and saturation adjustments

--- src/main/java/net/pneumono/locator_lodestones/server/mixin/CompassItemMixin.java
+++ src/main/java/net/pneumono/locator_lodestones/server/mixin/CompassItemMixin.java
@@ -8,7 +8,9 @@ import net.minecraft.item.CompassItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.ColorHelper;
 import net.minecraft.util.math.GlobalPos;
+import net.pneumono.locator_lodestones.server.ColorChromaHelper;
 
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -25,11 +27,15 @@ public abstract class CompassItemMixin {
     private <T> T setLodestoneComponents(ItemStack stack, ComponentType<T> type, T value) {
         if (value instanceof LodestoneTrackerComponent tracker) {
             GlobalPos pos = tracker.target().get();
-            MapColorComponent mapColor = stack.get(DataComponentTypes.MAP_COLOR);
-            String key = "lodestone_" + pos; if (mapColor !=null) key += "_" + mapColor.rgb();
-            int color = ColorHelper.withBrightness(ColorHelper.withAlpha(255, key.hashCode()), 0.9F);
-            stack.set(DataComponentTypes.MAP_COLOR, new MapColorComponent(color));
+            MapColorComponent mapColor = pos.equals(getLodestoneTarget(stack)) ? stack.get(DataComponentTypes.MAP_COLOR) : null;
+            int color = mapColor == null ? ColorHelper.withBrightness(pos.hashCode(), 0.9F)
+                                         : ColorChromaHelper.cycleHueSaturation(mapColor.rgb());
+            stack.set(DataComponentTypes.MAP_COLOR, new MapColorComponent(ColorHelper.withAlpha(255,color)));
         }
-        return stack.set(type, value);   
+        return stack.set(type, value);
+    }
+    private static @Nullable GlobalPos getLodestoneTarget(ItemStack stack) {
+        LodestoneTrackerComponent tracker = stack.get(DataComponentTypes.LODESTONE_TRACKER);
+        return tracker != null ? tracker.target().get() : null;
     }
 }
