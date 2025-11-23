f4662d7f281b5b8b2601da4285bc1ad8d09da049
fabien-gigante <fabien.gigante@free.fr>
2025-11-23 13:27:43 +0100

    Fix saturation

--- src/main/java/net/pneumono/locator_lodestones/server/ColorChromaHelper.java
+++ src/main/java/net/pneumono/locator_lodestones/server/ColorChromaHelper.java
@@ -41,7 +41,7 @@ public class ColorChromaHelper {
         int r = (color >> 16) & 0xff, g = (color >> 8) & 0xff, b = color & 0xff;
         float[] hsb = new float[3]; rgbToHsb(r, g, b, hsb);
         float h = (hsb[0] + HUE_STEP) % 1f;
-        float s = (float) Math.pow((hsb[1] * hsb[1] + SATURATION_STEP) % 1f, 0.5f);
+        float s = (float) Math.sqrt((hsb[1] * hsb[1] + SATURATION_STEP) % 1f);
         return hsbToRgb(h, s, hsb[2]);
     }
 
