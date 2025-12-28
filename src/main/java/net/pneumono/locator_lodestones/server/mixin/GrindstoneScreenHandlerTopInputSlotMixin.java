bb9818d3f7ed0f6ab07fb3e7b2d3bc863dda153f
fabien-gigante <fabien.gigante@free.fr>
2025-12-28 21:41:28 +0100

    use ModifyReturnValue

--- src/main/java/net/pneumono/locator_lodestones/server/mixin/GrindstoneScreenHandlerTopInputSlotMixin.java
+++ src/main/java/net/pneumono/locator_lodestones/server/mixin/GrindstoneScreenHandlerTopInputSlotMixin.java
@@ -6,7 +6,8 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
